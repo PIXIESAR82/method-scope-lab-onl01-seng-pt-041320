6feda498e48fc3e78c378139241af466827f19ca
sarah hussain <sarahhussain17@yahoo.com>
2020-04-23 00:08:39 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -6,7 +6,6 @@ end
 ## Comment the below back in to see method scope in action!
 
 
-describe "#catch_phrase" do 
-it "puts out a catch phrase" do 
+describe "#catch_phrase" do
+it "puts out a catch phrase" do
 phrase = "It's-a me, Mario!
-
